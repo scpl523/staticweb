ce670ea713f25327faf882354a12d46252e40a62
JoshuaLeoSmith <joshualeosmith1@gmail.com>
2023-11-28 18:33:50 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { TaskbarComponent } from './components/taskbar/taskbar.component';
+import { CountdownComponent } from './components/countdown/countdown.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TaskbarComponent,
+    CountdownComponent
   ],
   imports: [
     BrowserModule,
